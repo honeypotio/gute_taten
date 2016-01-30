f726c866965c18747a8f46026385ecfe9688c68e
Hugo Duksis <duksis@gmail.com>
2016-01-30 22:49:07 +0100

    adds proper mix task

---
+++ lib/mix/tasks/taten.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Taten do
+
+  @moduledoc """
+
+    Lists the contributions of a github profile
+
+    Usage:
+
+      mix taten duksis # lists contributions of user duksis
+  """
+
+  @shortdoc "Lists the contributions of a github profile"
+
+  use Mix.Task
+
+  def run([]), do: usage
+  def run(["--help"]), do: run([])
+  def run(["-h"]), do: run([])
+  def run([arg]) do
+    arg |> GuteTaten.retrieve |> IO.puts
+  end
+  def run(_), do: run([])
+
+  defp usage do
+    IO.puts @moduledoc
+  end
+end
