acf171d3193b90ba4871f9a2a3243ee000c69027
Hugo Duksis <duksis@gmail.com>
2016-03-09 19:03:06 +0100

    use tentacat from upstream github

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule GuteTaten.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [#{:tentacat, "~> 0.2"},
-      {:tentacat, github: "duksis/tentacat", branch: "user_events"},
+      {:tentacat, github: "edgurgel/tentacat"},
      {:excoveralls, "~> 0.4", only: :test},
      {:mock, "~> 0.1.1", only: :test},
      {:exvcr, "~> 0.6", only: :test} ]
