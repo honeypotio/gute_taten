0ae871b1cce9c0fd7c4ffcb018faecd8c212ff73
Hugo Duksis <duksis@gmail.com>
2016-06-07 22:56:18 +0200

    Make the contribution rule to only count accepted pr's

--- lib/gute_taten/giving_back_the_love.ex
+++ lib/gute_taten/giving_back_the_love.ex
@@ -1,9 +1,34 @@
 defmodule GuteTaten.GivingBackTheLove do
   def call(github_username, api \\ GuteTaten.Api) do
     api.call(Tentacat.Users.Events, :list_public, github_username)
-    |> Enum.filter(fn(x) -> Map.fetch!(x, "type") == "PullRequestEvent" end)
-    |> Enum.filter(fn(x) -> case x do %{"payload" => %{"action" => "opened"}} -> true ; _ -> false end; end)
-    #   |> Enum.filter(fn(x) -> case x do %{"payload" => %{"pull_request" => %{"base" => %{"repo" => %{"owner" => %{"name" => github_username}}}}}} -> false ; _ -> true end; end)
+    |> Enum.filter(&pull_request_event/1)
+    |> Enum.filter(&open_pr/1)
+    |> Enum.filter(&exclude_user(&1, github_username))
     |> Enum.map(fn(x) -> %{ name: "Is contributing back", reference: Map.fetch!(Map.fetch!(Map.fetch!(x, "payload"), "pull_request"), "html_url")} end)
+    |> Enum.filter(&has_been_merged(&1, api))
+  end
+
+  defp has_been_merged(%{reference: "https://github.com/" <> path}, api) do
+    [user, repo, _, nr] = String.split(path, "/")
+    case api.call(Tentacat.Pulls, :has_been_merged, [user, repo, nr]) do
+      {204, _} -> true
+      _ -> false
+    end
+  end
+
+  defp open_pr(%{"payload" => %{"action" => "opened"}}), do: true
+  defp open_pr(_), do: false
+
+  defp pull_request_event(%{"type" => "PullRequestEvent"}), do: true
+  defp pull_request_event(_), do: false
+
+  defp exclude_user(event, user) do
+    path = "https://github.com/" <> user
+    path_size = byte_size(path)
+    case event do
+      # https://github.com/elixir-lang/elixir/issues/3837
+      %{"payload" => %{"pull_request" => %{"html_url" =>  <<^path::binary-size(path_size)>> <> _}}} -> false
+      _ -> true
+    end
   end
 end
--- test/gute_taten/giving_back_the_love_test.exs
+++ test/gute_taten/giving_back_the_love_test.exs
@@ -6,23 +6,87 @@ defmodule GuteTaten.GivingBackTheLoveTest do
   def api_double_module(name, value) do
     module = Module.concat(__MODULE__, name)
     contents = quote do
-      def call(_module, _fun, _args), do: unquote(Macro.escape(value))
+      def call(mod, fun, args) do
+        case [mod, fun, args] do
+          [Tentacat.Pulls, :has_been_merged, [_, "devise", "1"]] -> {204, "Merged"}
+          [Tentacat.Pulls, :has_been_merged, _] -> {404, "Not Found"}
+          _ -> unquote(Macro.escape(value))
+        end
+      end
     end
     Module.create(module, contents, Macro.Env.location(__ENV__))
     module
   end
 
-  test "filters PR's" do
+  test "filters non PR's" do
     api_response = [
       %{
         "type" => "PullRequestEvent",
-        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/RoxasShadow/devise_invitations"}}
-      }
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/platformatec/devise/pull/1"}}
+      },
+      %{"type" => "Event"}
     ]
     api_double = api_double_module("FiltersPRs", api_response)
     expected_output = [%{
       name: "Is contributing back",
-      reference: "https://github.com/RoxasShadow/devise_invitations"
+      reference: "https://github.com/platformatec/devise/pull/1"
+    }]
+    assert GuteTaten.GivingBackTheLove.call("RoxasShadow", api_double) == expected_output
+  end
+
+  test "filters PR closed events" do
+    api_response = [
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/platformatec/devise/pull/1"}}
+      },
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "closed"}
+      }
+    ]
+    api_double = api_double_module("FiltersPROpenedEvents", api_response)
+    expected_output = [%{
+      name: "Is contributing back",
+      reference: "https://github.com/platformatec/devise/pull/1"
+    }]
+    assert GuteTaten.GivingBackTheLove.call("RoxasShadow", api_double) == expected_output
+  end
+
+  test "filters PR's to own repos" do
+    api_response = [
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/platformatec/devise/pull/1"}}
+      },
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/RoxasShadow/devise/pull/1"}}
+      }
+    ]
+    api_double = api_double_module("FiltersOwnPRs", api_response)
+    expected_output = [%{
+      name: "Is contributing back",
+      reference: "https://github.com/platformatec/devise/pull/1"
+    }]
+    assert GuteTaten.GivingBackTheLove.call("RoxasShadow", api_double) == expected_output
+  end
+
+  test "filters merged PR" do
+    api_response = [
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/platformatec/devise/pull/1"}}
+      },
+      %{
+        "type" => "PullRequestEvent",
+        "payload" => %{"action" => "opened", "pull_request" => %{"html_url" => "https://github.com/platformatec/devise/pull/2"}}
+      }
+    ]
+    api_double = api_double_module("FiltersMergedPRs", api_response)
+    expected_output = [%{
+      name: "Is contributing back",
+      reference: "https://github.com/platformatec/devise/pull/1"
     }]
     assert GuteTaten.GivingBackTheLove.call("RoxasShadow", api_double) == expected_output
   end
