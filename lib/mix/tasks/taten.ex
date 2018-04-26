03b752f45f0f393a3fab0606ee08190789783047
Kevin Butler <kb@honeypot.io>
2018-04-26 16:56:13 +0200

    Revert "Change .retrieve function to return a stream"
    This reverts commit 28c9d17f19d8a49f9f43292acaf36129241175e9.

--- lib/mix/tasks/taten.ex
+++ lib/mix/tasks/taten.ex
@@ -21,15 +21,11 @@ defmodule Mix.Tasks.Taten do
     |> Enum.each(&Application.ensure_all_started/1)
     Tentacat.start
     Githubarchive.Repo.start_link
-    arg |> GuteTaten.retrieve |> Enum.map(&print/1)
+    arg |> GuteTaten.retrieve |> IO.puts
   end
   def run(_), do: run([])
 
   defp usage do
     IO.puts @moduledoc
-  end
-
-  defp print(deed) do
-    JSX.encode!(deed) |> JSX.prettify! |> IO.puts
   end
 end
